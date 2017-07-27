f94fd71335b296c84beb5353eaba48f835f4c02d
Ali Khan <akhanf@gra-login2.graham.sharcnet>
2017-07-27 17:03:47 -0400

    updated cfg file

--- config_graham-sharcnet.sh
+++ config_graham-sharcnet.sh
@@ -2,9 +2,9 @@
 
 export remote_user="akhanf"
 export remote_host="graham.sharcnet.ca"
-export remote_singularity_dir="/project/akhanf/singularity"
+export remote_singularity_dir="/home/akhanf/project/singularity"
 export local_bids_root="~/khangrp/datasets-bids"
-export remote_bids_root="/project/akhanf/datasets-bids"
-export slurmopts="--time=36:00:00 --mem=16000 --account=rrg-akhanf"
+export remote_bids_root="/home/akhanf/project/datasets-bids"
+export slurmopts="--time=24:00:00 --mem=64000 --account=rrg-akhanf --cpus-per-task=16" #default, requests 1/2 node for a day -- 16 cores, 64GB ram (default node is 32 cores, 128GB ram)
 
 
